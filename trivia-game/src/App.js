32fa9b63339cc23f763a975ef93ef77f4c300afb
Alex Curtin <alexcurtin@Alex-Curtins-MacBook-Pro-20.local>
2019-06-28 10:56:50 -0400

    create, test api call functions

--- trivia-game/src/App.js
+++ trivia-game/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom';
+import { getCategories, getQuestion } from './services/api-helper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      question: ''
+    }
+  }
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({
+      categories: categories,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>something is working, at least</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ trivia-game/src/services/api-helper.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+export const getCategories = async () => {
+  const resp = await axios.get('https://opentdb.com/api_category.php');
+  return resp.data.trivia_categories;
+}
+
+export const getQuestion = async (cat) => {
+  const resp = await axios.get(`https://opentdb.com/api.php?amount=1&category=${cat}`);
+  return resp.data.results[0];
+}
